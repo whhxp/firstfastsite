b46daa2d689aa2ba3cef02e2badb011fa3c3a20c
jinlei.hb <jinlei.hb@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2009-12-17 01:54:38 +0000

    范乐

--- beginFS/src/org/apachechina/firstsite/begin/fanle/MyMessageFormat.java
+++ beginFS/src/org/apachechina/firstsite/begin/fanle/MyMessageFormat.java
@@ -1,10 +1,35 @@
 package org.apachechina.firstsite.begin.fanle;
 
 public class MyMessageFormat {
-	public static String format(String pattern,Object[] args)
-	{
-		
-		return "ÎÒÊÇ"+args[0]+",ÄêÁä"+args[1];
-		
+	public static String format(String pattern, Object[] args) {
+		String sub[]=new String[pattern.length()];
+		String back="";
+		for (int i = 0; i < pattern.length(); i++) {
+			sub[i]=pattern.substring(i, i + 1);
+		}
+		for (int i = 0; i < sub.length; i++) {
+			if(sub[i].equals("0"))
+			{
+				back+=args[0];
+			}
+			else if(sub[i].equals("1"))
+			{
+				back+=args[1];
+			}
+			else if(sub[i].equals("2"))
+			{
+				back+=args[2];
+			}
+			else if(sub[i].equals("{")||sub[i].equals("}"))
+			{
+				back+="";
+			}
+			else 
+			{
+				back+=sub[i];
+			}
+
+		}
+		return back;
 	}
 }
