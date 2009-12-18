46ea91e638b10e09aa55cf2c314b7fdea9853280
renzhuowei <renzhuowei@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2009-12-18 01:17:53 +0000

    新实现的Format

--- beginFS/test/org/apachechina/firstsite/begin/renzhuowei/MyMessageFormatAndOtherTest.java
+++ beginFS/test/org/apachechina/firstsite/begin/renzhuowei/MyMessageFormatAndOtherTest.java
@@ -1,14 +1,11 @@
 package org.apachechina.firstsite.begin.renzhuowei;
-
-import java.text.MessageFormat;
-
 import junit.framework.TestCase;
 
 public class MyMessageFormatAndOtherTest extends TestCase{
 	public void testMessageFormat_format_base()
 	{
-		String execept=MyMessageFormat.format("我是{0},年龄{1}", new Object[]{"倪森",24});
-		String real=org.apachechina.firstsite.begin.jinlei.MyMessageFormat.format("我是{0},年龄{1}", new Object[]{"倪森",24});
+		String execept=MyMessageFormat.format("我是{0},年龄{1}", new Object[]{"天啊",24});
+		String real=org.apachechina.firstsite.begin.jinlei.MyMessageFormat.format("我是{0},年龄{1}", new Object[]{"天啊",24});
 		
 		
 		assertEquals(execept, real);
