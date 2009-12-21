5b57e6253b2699ae8670f3ab32273655cc089189
nisen.cn <nisen.cn@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2009-12-21 03:53:11 +0000

    上传了新的测试

--- beginFS/test/org/apachechina/firstsite/begin/fanle/MyMessageFormatTest.java
+++ beginFS/test/org/apachechina/firstsite/begin/fanle/MyMessageFormatTest.java
@@ -9,12 +9,18 @@ public class MyMessageFormatTest extends TestCase {
 	public void testMessageFormat_format_base() {
 		String real = MyMessageFormat.format("我是{0},年龄{1},{0}", new Object[] {
 				"倪森", 24 });
-		assertEquals("我是倪森,年龄24", real);
+		assertEquals("我是倪森,年龄24,倪森", real);
 	}
 
 	public void testMessageFormat_format_other() {
 		String real = MyMessageFormat.format("我是{0},年龄{1},{0}", new Object[] {
 				"jinlei", 25 });
-		assertEquals("我是jinlei,年龄25", real);
+		assertEquals("我是jinlei,年龄25,jinlei", real);
+	}
+	
+	public void testMessageFormat_format_other3() {
+		String real = MyMessageFormat.format("我是{0},年龄{1},{2},{3}", new Object[] {
+				"jinlei", 25 ,78,"test"});
+		assertEquals("我是jinlei,年龄25,78,test", real);
 	}
 }
