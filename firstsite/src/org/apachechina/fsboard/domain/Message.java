d50198958495b6789745f0c6b3181ac211a062e2
nisen.cn <nisen.cn@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2009-12-21 09:16:44 +0000

    提交一个基本的模型

---
+++ firstsite/src/org/apachechina/fsboard/domain/Message.java
@@ -0,0 +1,39 @@
+package org.apachechina.fsboard.domain;
+
+public class Message {
+	String id;
+	String title;
+	String context;
+	String author;
+	
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContext() {
+		return context;
+	}
+	public void setContext(String context) {
+		this.context = context;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	} 
+	
+	
+	
+	
+}
---
+++ firstsite/src/org/apachechina/fscore/api/UserManager.java
@@ -0,0 +1,24 @@
+package org.apachechina.fscore.api;
+
+import org.apachechina.fscore.domain.User;
+
+
+
+/**
+ * 用户管理
+ * @author nisen.cn@gmail.com
+ */
+public class UserManager {
+	
+	
+	
+
+	/**
+	 * 得到当前用户
+	 * @return
+	 */
+	public User getCurrentUser(){
+		return new User("1","test");		
+	}
+	
+}
