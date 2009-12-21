0a2a919e0bff0a3512f61a8f052e842f1699ff79
nisen.cn <nisen.cn@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2009-12-21 03:43:28 +0000

    把所有的单元测试串起来

---
+++ beginFS/test/org/apachechina/firstsite/begin/jinlei/AllTests.java
@@ -0,0 +1,16 @@
+package org.apachechina.firstsite.begin.jinlei;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("½ùÀ×µÄ²âÊÔ");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(MyMessageFormatTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
