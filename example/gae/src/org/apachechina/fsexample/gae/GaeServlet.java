7e42be92de0a8252d7752a1e332d329c5fe54a7e
nisen.cn <nisen.cn@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2009-12-23 03:27:21 +0000

    更改了时间

--- example/gae/src/org/apachechina/fsexample/gae/GaeServlet.java
+++ example/gae/src/org/apachechina/fsexample/gae/GaeServlet.java
@@ -1,6 +1,8 @@
 package org.apachechina.fsexample.gae;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -8,6 +10,7 @@ public class GaeServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		
+		resp.getWriter().println("google service time" + new Date());
 	}
 }
