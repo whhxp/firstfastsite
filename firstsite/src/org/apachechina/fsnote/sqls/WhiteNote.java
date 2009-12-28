5829a0497348a476a6682607cefc91fa09ab2ae5
renzhuowei <renzhuowei@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2009-12-28 04:20:27 +0000

    表的添加

---
+++ firstsite/src/org/apachechina/fsnote/sqls/WhiteNote.java
@@ -0,0 +1,29 @@
+package org.apachechina.fsnote.sqls;
+
+public class WhiteNote
+{
+	private int id;
+	private String title;
+	private String meirong;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return title;
+	}
+	public void setName(String title) {
+		this.title = title;
+	}
+	public String getPwd() {
+		return meirong;
+	}
+	public void setPwd(String meirong) {
+		this.meirong = meirong;
+	}
+	
+	
+}
