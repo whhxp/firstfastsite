db5b7157633bb250a894b5b3472d1671bd4a741a
gaoxin2000 <gaoxin2000@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2009-12-28 07:29:21 +0000

    Uesr类

---
+++ trunk/firstsite/src/org/apachechina/fschatroom/domain/ChatUserList.java
@@ -0,0 +1,29 @@
+//@charset "UTF-8";
+package org.apachechina.fschatroom.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apachechina.fscore.domain.User;
+
+/**
+ * @author gaoxin2000@gmail.com
+ *
+ */
+public class ChatUserList {
+	//单例
+	private static ChatUserList instance = null;
+
+	public static synchronized ChatUserList getInstance() {
+		if (instance == null)
+			instance = new ChatUserList();
+		return instance;
+	}
+	
+	List<User> chatUsers=new ArrayList<User>();
+	
+	public List<User> add(User user)
+	{
+		chatUsers.add(user);
+		return chatUsers;
+	}
+}
