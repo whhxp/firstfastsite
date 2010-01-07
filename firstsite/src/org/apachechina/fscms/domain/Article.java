6f7e33c606cbedc50a3b5e591fca9a139060b96d
nisen.cn <nisen.cn@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2010-01-07 03:59:02 +0000

    cms加上了框架

---
+++ firstsite/src/org/apachechina/fscms/domain/Article.java
@@ -0,0 +1,28 @@
+//@charset "UTF-8"
+package org.apachechina.fscms.domain;
+
+public class Article {
+	long id;
+	String title;
+	
+	
+	public Article(String title) {
+		super();
+		this.title = title;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+}
