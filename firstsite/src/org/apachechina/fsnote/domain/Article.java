d54b3227a225cf225bb3b7f0ccaf0febe4566fd6
renzhuowei <renzhuowei@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2010-01-08 04:33:27 +0000

    note的更新

---
+++ firstsite/src/org/apachechina/fsnote/domain/Article.java
@@ -0,0 +1,52 @@
+package org.apachechina.fsnote.domain;
+
+import java.util.Date;
+
+public class Article {
+
+	private Long id;
+	private String title; // 文章的主题
+	private String gut;// 文章的内容
+	private Date date;// 发表时间
+
+	public Article() {
+	}
+
+	public Article(String title, String gut, Date date) {
+		this.title = title;
+		this.gut = gut;
+		this.date = date;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGut() {
+		return gut;
+	}
+
+	public void setGut(String gut) {
+		this.gut = gut;
+	}
+}
