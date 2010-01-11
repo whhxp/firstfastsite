a76f676b9b94121fd5524062a3105c3c4be14129
nisen.cn <nisen.cn@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2010-01-11 02:34:20 +0000

    增加了List页面

--- trunk/firstsite/src/org/apachechina/fscms/domain/Article.java
+++ trunk/firstsite/src/org/apachechina/fscms/domain/Article.java
@@ -1,29 +1,56 @@
 //@charset "UTF-8"
 package org.apachechina.fscms.domain;
 
+import java.util.List;
+import java.util.Vector;
+
 public class Article {
 	long id;
 	String title;
-	 
-	public Article(){}
-	
+
+	public Article() {
+	}
+
 	public Article(String title) {
 		super();
 		this.title = title;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
-	
+
+	// *系统内置数据*//
+
+	/**
+	 * @return 一条数据
+	 */
+	static public Article example() {
+		return new Article("tata");
+	}
+
+	/**
+	 * 多条数据
+	 * @return
+	 */
+	static public List<Article> examples() {
+		List<Article> list = new Vector<Article>();
+		list.add(new Article("第一篇文章题目"));
+		list.add(new Article("第二篇文章题目"));
+		list.add(new Article("第三篇文章题目"));
+		return list;
+	}
+
 }
