eafc4cf4e5fafbf691da69cc43bb6556db36813e
LongFei.cn <LongFei.cn@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2010-01-11 10:26:34 +0000

    添加修改数据库表格

---
+++ firstsite/src/org/apachechina/fsphoto/dao/PicDao.java
@@ -0,0 +1,12 @@
+package org.apachechina.fsphoto.dao;
+
+import java.util.List;
+
+import org.apachechina.fsphoto.domain.Pic;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class PicDao extends HibernateDaoSupport {
+	public List<Pic> getList() {
+		return getHibernateTemplate().find("from Pic");
+	}
+}
