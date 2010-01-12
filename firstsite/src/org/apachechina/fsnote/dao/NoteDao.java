117d81083d047bee96f5e04a3ee6c5a93c69b188
renzhuowei <renzhuowei@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2010-01-12 01:39:36 +0000

    配制的部分完成

--- firstsite/src/org/apachechina/fsnote/dao/NoteDao.java
+++ firstsite/src/org/apachechina/fsnote/dao/NoteDao.java
@@ -3,15 +3,15 @@ package org.apachechina.fsnote.dao;
 
 import java.util.List;
 
-import org.apachechina.fsnote.domain.NoteList;
+import org.apachechina.fsnote.domain.Note;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 /**
  * @author Administrator
  * 
  */
-public class ArticleDao extends HibernateDaoSupport {
-	public List<NoteList> getList() {
+public class NoteDao extends HibernateDaoSupport {
+	public List<Note> getList() {
 		return getHibernateTemplate().find("from Article");
 
 	}
