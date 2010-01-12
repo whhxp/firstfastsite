b438ab24395236bf32e85dcce92647975b234850
renzhuowei <renzhuowei@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2010-01-12 01:39:36 +0000

    配制的部分完成

---
+++ trunk/firstsite/src/org/apachechina/fsnote/action/NoteList.java
@@ -0,0 +1,29 @@
+//@charset "UTF-8";
+package  org.apachechina.fsnote.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+
+import org.apachechina.fsnote.dao.NoteDao;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apachechina.fsnote.domain.Note;
+import com.opensymphony.xwork2.Action;
+/**
+ * @author Administrator
+ * 
+ */
+public class NoteList extends ActionSupport implements Action
+{
+
+	public String execute() throws Exception {
+		System.out.println("test here");
+		HttpServletRequest request = ServletActionContext.getRequest();		
+		return SUCCESS;	
+		
+	}
+
+}
--- trunk/firstsite/src/org/apachechina/fsnote/domain/Note.java
+++ trunk/firstsite/src/org/apachechina/fsnote/domain/Note.java
@@ -1,18 +1,22 @@
 package org.apachechina.fsnote.domain;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Vector;
 
-public class NoteList {
+import org.apachechina.fscms.domain.Article;
+
+public class Note {
 
 	private Long id;
 	private String title; // 文章的主题
 	private String gut;// 文章的内容
 	private Date date;// 发表时间
 
-	public NoteList() {
+	public Note() {
 	}
 
-	public NoteList(String title, String gut, Date date) {
+	public Note(String title, String gut, Date date) {
 		this.title = title;
 		this.gut = gut;
 		this.date = date;
@@ -48,5 +52,26 @@ public class NoteList {
 
 	public void setGut(String gut) {
 		this.gut = gut;
+	}
+	
+	// *系统内置数据*//
+
+	/**
+	 * @return 一条数据
+	 */
+	static public Note example() {
+		return new Note("title","11111",new Date());
+	}
+
+	/**
+	 * 多条数据
+	 * @return
+	 */
+	static public List<Note> examples() {
+		List<Note> list = new Vector<Note>();
+		list.add(new Note("title","11111",new Date()));
+		list.add(new Note("title2","22222",new Date()));
+		list.add(new Note("title3","33333",new Date()));
+		return list;
 	}
 }
