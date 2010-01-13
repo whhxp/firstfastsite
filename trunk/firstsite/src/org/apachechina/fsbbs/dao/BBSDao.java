3c189af1734b0e646dc2a8e5389e85f77fca6d75
fanle004 <fanle004@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2010-01-13 02:23:55 +0000

    论坛action的更新

--- trunk/firstsite/src/org/apachechina/fsbbs/dao/BBSDao.java
+++ trunk/firstsite/src/org/apachechina/fsbbs/dao/BBSDao.java
@@ -4,17 +4,31 @@ package org.apachechina.fsbbs.dao;
 import java.util.List;
 
 import org.apachechina.fsbbs.domain.BBS;
+import org.apachechina.fsbbs.domain.Word;
 import org.apachechina.fsboard.domain.Board;
 
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-public class BBSDao extends HibernateDaoSupport{
-	
-	
-	public List<BBS> getList(){
-		
-		return getHibernateTemplate().find("from BBS");		
-		
+public class BBSDao extends HibernateDaoSupport {
+
+	// 查询出bbs所有的内容
+	public List<BBS> getList() {
+		return getHibernateTemplate().find("from BBS");
 	}
-	
+
+	// 在数据库插入一个BBS
+	public void addbbs(BBS bbs) {
+		getHibernateTemplate().save(bbs);
+	}
+
+	// 得到数据库一条内容
+	public BBS getOne(long id) {
+		BBS bbs = (BBS) getHibernateTemplate().get(BBS.class, id);
+		return bbs;
+	}
+
+	// 查询出留言表所有的内容
+	public List<Word> getword() {
+		return getHibernateTemplate().find("from Word");
+	}
 }
