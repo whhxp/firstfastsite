951735afebdb0bf1f6c4a7100e170d01777f9822
renzhuowei <renzhuowei@512cd92e-e866-11de-a5fe-b7516cc0fd57>
2010-01-13 04:16:03 +0000

    添加Groups表

---
+++ trunk/firstsite/src/org/apachechina/fsnote/domain/Groups.java
@@ -0,0 +1,27 @@
+package org.apachechina.fsnote.domain;
+
+public class Groups {
+	private int id;
+	private String name;//组名
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Groups(){}
+	
+	
+	
+}
